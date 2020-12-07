5d5b488a55999c80768c6a79c0da8fda15675c1b
bashforger <bash.forger@gmail.com>
2020-12-08 02:19:43 +0500

    Finished prototype

--- lib/ui.rb
+++ lib/ui.rb
@@ -12,7 +12,7 @@ class UI
   def ui
     puts @font.write('Yo Scrapper!').red
     category = @prompt.select('Select catogory#', controller('init'), filter: true)
-    scrap(category)
+    controller(category)
   end
 
   private
@@ -23,80 +23,149 @@ class UI
       puts TTY::Link.link_to("Fetching catogories from#{' TradingView'.blue.bold}", @_root_addr.blue)
       @spinner.auto_spin
       @target = 'a.tv-feed-widget__title-link'
-      @out = scrap(str, [@_root_addr, @target])
+      @out = init([@_root_addr, @target])
     when 'Indices'
-      puts 'Indices'
+      @ref = @_root_addr + @_address[@_name.index(str)][8..-1]
+      indices(@ref)
+    when 'Futures'
+      @ref = @_root_addr + @_address[@_name.index(str)][8..-1]
+      futures(@ref)
+    when 'Currencies'
+      @ref = @_root_addr + @_address[@_name.index(str)][8..-1]
+      currencies(@ref)
+    when 'Bonds'
+      @ref = @_root_addr + @_address[@_name.index(str)][8..-1]
+      bonds(@ref)
+    when 'Stocks'
+      @ref = "#{@_root_addr}/stocks-usa/market-movers-large-cap/"
+      stocks(@ref)
+    when 'Cryptocurrencies'
+      @ref = @_root_addr + @_address[@_name.index(str)][8..-1]
+      cryptocurrencies
+    when 'exit'
+      abort('yeah')
     end
     @spinner.success
     @out
   end
 
-  def scrap(category, args = nil)
-    case category
-    when 'init'
-      init(args)
-      # s = @_root_addr + @_address[1][8..-1]
-      # puts s
-      # puts @_name.inspect, @_address.inspect
-    when 'Indices'
-      indices(args)
-    when 'Futures'
-      futures(args)
-    when 'Currencies'
-      currencies(args)
-    when 'Bonds'
-      bonds(args)
-    when 'Stocks'
-      stocks(args)
-    when 'Cryptocurrencies'
-      cryptocurrencies
-    when 'exit'
-      abort('yeah')
-    end
-  end
-
   def init(args)
-    @doc = Nokogiri::HTML(URI.open(args[0]))
+    @doc = Nokogiri::HTML(URI.parse(args[0]).open)
     @_name = @doc.css(args[1]).text.split
     @_address = @doc.css(args[1]).map { |link| link['href'] }
     @_name
   end
 
   def indices(_args = nil)
-    puts 'Indices'.bold.blue
+    @out = []
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
+    @doc = Nokogiri::HTML(URI.parse(@ref).open)
+    @filter = 'tr.tv-data-table__row'
+    @header = %w[NAME PRICE CHANGE-% HIGH LOW RECOMMENDATION]
+    @doc.css(@filter).each do |i|
+      @name = i.css('a[class="tv-screener__symbol"]').text
+      @price = i.css('td.tv-screener-table__cell--big')[1].text
+      @change = i.css('td.tv-screener-table__cell--big')[2].text
+      @high = i.css('td.tv-screener-table__cell--big')[4].text
+      @low = i.css('td.tv-screener-table__cell--big')[5].text
+      @recommendation = i.css('td.tv-screener-table__cell--big')[6].text
+      @out << [@name, @price, @change, @high, @low, @recommendation]
+    end
+    @spinner.success
+    options_controller([@header, @out])
   end
 
   def futures(_args = nil)
-    puts 'futures'.bold.blue
+    @out = []
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
+    @doc = Nokogiri::HTML(URI.parse(@ref).open)
+    @filter = 'tr.tv-data-table__row'
+    @header = %w[NAME PRICE CHANGE-% HIGH LOW RECOMMENDATION]
+    @doc.css(@filter).each do |i|
+      @name = i.css('a[class="tv-screener__symbol"]').text
+      @price = i.css('td.tv-screener-table__cell--big')[1].text
+      @change = i.css('td.tv-screener-table__cell--big')[2].text
+      @high = i.css('td.tv-screener-table__cell--big')[4].text
+      @low = i.css('td.tv-screener-table__cell--big')[5].text
+      @recommendation = i.css('td.tv-screener-table__cell--big')[6].text
+      @out << [@name, @price, @change, @high, @low, @recommendation]
+    end
+    @spinner.success
+    options_controller([@header, @out])
   end
 
   def currencies(_args = nil)
-    puts 'currencies'.bold.blue
+    @out = []
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
+    @doc = Nokogiri::HTML(URI.parse(@ref).open)
+    @filter = 'tr.tv-data-table__row'
+    @header = %w[NAME PRICE CHANGE-% BID ASK HIGH LOW RECOMMENDATION]
+    @doc.css(@filter).each do |i|
+      @name = i.css('a[class="tv-screener__symbol"]').text
+      @price = i.css('td.tv-screener-table__cell--big')[1].text
+      @change = i.css('td.tv-screener-table__cell--big')[2].text
+      @bid = i.css('td.tv-screener-table__cell--big')[4].text
+      @ask = i.css('td.tv-screener-table__cell--big')[5].text
+      @high = i.css('td.tv-screener-table__cell--big')[6].text
+      @low = i.css('td.tv-screener-table__cell--big')[7].text
+      @recommendation = i.css('td.tv-screener-table__cell--big')[8].text
+      @out << [@name, @price, @change, @bid, @ask, @high, @low, @recommendation]
+    end
+    @spinner.success
+    options_controller([@header, @out])
   end
 
   def bonds(_args = nil)
-    puts 'bonds'.bold.blue
-  end
-
-  def stocks
-    @ref = 'https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/'
+    @out = []
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
     @doc = Nokogiri::HTML(URI.parse(@ref).open)
     @filter = 'tr.tv-data-table__row'
+    @header = %w[NAME COUPON MATURITY-DATE YIELD CHANGE-% HIGH LOW RECOMMENDATION]
     @doc.css(@filter).each do |i|
-      @name = i.css('a[class="tv-screener__symbol"]').text.bold
-      @cap = i.css('td.tv-screener-table__cell--big')[6].text.bold
-      @price = i.css('td.tv-screener-table__cell--big')[1].text.bold
-      @vol = i.css('td.tv-screener-table__cell--big')[5].text.bold
-      @recommendation = i.css('td.tv-screener-table__cell--big')[4].text.bold
-      @sector = i.css('td.tv-screener-table__cell--big')[10].text.bold
-      puts " #{@name} #{@cap} #{@price} #{@vol} #{@recommendation} #{@sector}"
+      @name = i.css('a[class="tv-screener__symbol"]').text
+      @coupon = i.css('td.tv-screener-table__cell--big')[1].text
+      @maturity = i.css('td.tv-screener-table__cell--big')[2].text
+      @yield = i.css('td.tv-screener-table__cell--big')[3].text
+      @change = i.css('td.tv-screener-table__cell--big')[4].text
+      @high = i.css('td.tv-screener-table__cell--big')[6].text
+      @low = i.css('td.tv-screener-table__cell--big')[7].text
+      @recommendation = i.css('td.tv-screener-table__cell--big')[8].text
+      @out << [@name, @coupon, @maturity, @yield, @change, @high, @low, @recommendation]
     end
+    @spinner.success
+    options_controller([@header, @out])
   end
 
-  def cryptocurrencies
-    @spinner.auto_spin
+  def stocks(_args = nil)
     @out = []
-    @ref = 'https://www.tradingview.com/markets/cryptocurrencies/prices-all/'
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
+    @doc = Nokogiri::HTML(URI.parse(@ref).open)
+    @filter = 'tr.tv-data-table__row'
+    @header = %w[NAME MARKET-CAPITAL VOLUME PRICE CHANGE-% SECTOR PRICE_TO_EARNING_RATIO RATING]
+    @doc.css(@filter).each do |i|
+      @name = i.css('a[class="tv-screener__symbol"]').text
+      @cap = i.css('td.tv-screener-table__cell--big')[6].text
+      @vol = i.css('td.tv-screener-table__cell--big')[5].text
+      @price = i.css('td.tv-screener-table__cell--big')[1].text
+      @change = i.css('td.tv-screener-table__cell--big')[2].text
+      @sector = i.css('td.tv-screener-table__cell--big')[10].text
+      @pte = i.css('td.tv-screener-table__cell--big')[7].text
+      @rating = i.css('td.tv-screener-table__cell--big')[4].text
+      @out << [@name, @cap, @vol, @price, @change, @sector, @pte, @rating]
+    end
+    @spinner.success
+    options_controller([@header, @out])
+  end
+
+  def cryptocurrencies(_args = nil)
+    @out = []
+    puts TTY::Link.link_to("Fetching latest data from#{' TradingView'.blue.bold}", @ref.blue)
+    @spinner.auto_spin
     @doc = Nokogiri::HTML(URI.parse(@ref).open)
     @filter = 'tr.tv-data-table__row'
     @header = %w[NAME CAP PRICE AVAILABLE TOTAL VOLUME CHANGE]
